4faac1250011b082a703196ba00dd9d3bab50d2d
Guy <chamberlainwebberj@gmail.com>
2020-07-20 15:13:36 +0100

    Changed port number for mailing service

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,5 +105,5 @@ Rails.application.configure do
     enable_starttls_auto: true 
   }
 
-  config.action_mailer.default_url_options = { host: 'harrietcw.co.uk', port: 80 }
+  config.action_mailer.default_url_options = { host: 'harrietcw.co.uk', port: 22 }
 end
